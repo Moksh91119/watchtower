ed02a20a22a8ac19b6243ef9d222e55d6ba23669
Moksh91119 <jainmoksh03@gmail.com>
2026-09-14 21:11:43 +0530

    configure ssl for github actions worker

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -5,7 +5,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 export * from "./schema.js";
 
 const client = postgres(process.env.DATABASE_URL!, {
-  ssl: process.env.DATABASE_SSL === "true" ? "require" : undefined,
+  ssl: process.env.GITHUB_ACTIONS === "true" ? "require" : undefined,
 });
 
 export const db = drizzle(client);
